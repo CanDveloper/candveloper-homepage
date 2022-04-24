a35cc469e41b5ae6aacf5b5c598bb4cd8d2d6fb1
CanDveloper <jaimecaberocreus.business@gmail.com>
2022-04-24 22:02:05 +0200

    Bio and Hobbies added

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,28 @@
-import { Image, Container, Box, Heading, useColorModeValue, Link } from '@chakra-ui/react'
+import {
+    Image,
+    Container,
+    Box,
+    Heading,
+    useColorModeValue,
+    Link,
+    Button
+} from '@chakra-ui/react'
 import NextLink from 'next/link'
 import Section from '../components/section'
 import Paragraph from '../components/paragraph'
+import { ChevronRightIcon } from '@chakra-ui/icons'
+import { BioSection, BioYear } from '../components/bio'
 
 const Page = () => {
     return (
         <Container>
-            <Box borderRadius="lg" bg={useColorModeValue('whiteAlpha.500', 'whiteAlpha.200')} p={3} mb={6} align="center">
+            <Box
+                borderRadius="lg"
+                bg={useColorModeValue('whiteAlpha.500', 'whiteAlpha.200')}
+                p={3}
+                mb={6}
+                align="center"
+            >
                 Hello, I'm a full-stack developer based in EU!
             </Box>
 
@@ -37,18 +53,60 @@ const Page = () => {
                 </Box>
             </Box>
             <Section delay={0.1}>
-            <Heading as="h3" variant="section-title">
-                Work
-            </Heading>
-            <Paragraph>
-                Jaime is software engineer and a full-stack developer
-                based in La Coruña. 
-                <NextLink href="/works/inkdrop">
-                    <Link>
-                        TradeMarket
-                    </Link>
-                </NextLink>
-            </Paragraph>
+                <Heading as="h3" variant="section-title">
+                    Work
+                </Heading>
+                <Paragraph>
+                    Jaime is a software engineer and a full-stack developer
+                    based in La Coruña, Spain. He is very passionate about
+                    technologie so he decided to learn how to create digital
+                    products, following good engineering practices, from
+                    planning and designing to the implementation of final
+                    functional products. When not online, he enjoys playing
+                    sports like paddle or researching in other knowledge areas,
+                    mainly in the world of finance and investiments.
+                </Paragraph>
+                <Box align="center" my={5}>
+                    <NextLink href="/works">
+                        <Button
+                            rightIcon={<ChevronRightIcon />}
+                            colorScheme="teal"
+                        >
+                            My portfolio
+                        </Button>
+                    </NextLink>
+                </Box>
+            </Section>
+
+            <Section delay={0.2}>
+                <Heading as="h3" variant="section-title">
+                    Bio
+                </Heading>
+                <BioSection>
+                    <BioYear>1997</BioYear>
+                    Born in Santiago de Compostela, Spain.
+                </BioSection>
+                <BioSection>
+                    <BioYear>2021</BioYear>
+                    Completed the Computer Engineering Degree at Univeristy of
+                    La Coruña.
+                </BioSection>
+                <BioSection>
+                    <BioYear>2022</BioYear>
+                    Completing the Master's Degree in Computer Engineering at
+                    the University of La Coruña. Also working at Odeene
+                    Engineering.
+                </BioSection>
+            </Section>
+
+            <Section delay={0.3}>
+                <Heading as="h3" variant="section-title">
+                    I ❤
+                </Heading>
+                <Paragraph>
+                    Music, Writing, Traveling, Sports, Movies, Finances,
+                    Gastronomy
+                </Paragraph>
             </Section>
         </Container>
     )
